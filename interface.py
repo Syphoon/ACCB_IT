3d899716c2be3d35ae93fd28eef816d180d1fb81
Samuel <smvasconcelos11@gmail.com>
2021-02-03 21:28:27 -0300

    BUGFIX except_raise

--- interface.py
+++ interface.py
@@ -103,11 +103,11 @@ class Tread(threading.Thread):
 			
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-			print(exc_type, fname, exc_tb.tb_lineno)
-			print(e)
+			# print(exc_type, fname, exc_tb.tb_lineno)
+			# print(e)
 			self.err_log(str(exc_type), str(fname), str(exc_tb.tb_lineno), str(e))
-
 			self.exc = e 
+			self.except_raise()
 		
 
 class Interface:
